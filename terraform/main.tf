e8ecd4be4021fe73cbb072ef57835fb5b0f8ad81
Dum <tpcoea@gmail.com>
2026-04-23 22:43:55 +0700

    Change EC2 instance type from t2.micro to t3.medium

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,7 +63,7 @@ resource "aws_security_group" "devops_sg" {
 # EC2 Instance
 resource "aws_instance" "devops_ec2" {
   ami                    = "ami-0c02fb55956c7d316"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.medium"
   key_name               = "devops-final-key"
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
